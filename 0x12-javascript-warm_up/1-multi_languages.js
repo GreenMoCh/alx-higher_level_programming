03e79c959ea5557bcaeb8fd13f023f1aa44c1e23
GreenMoCh <moadchabaa0@gmail.com>
2023-11-13 13:08:18 -0800

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const line_1 = "C is fun";
+console.log(line_1);
+
+const line_2 = "Python is cool";
+console.log(line_2);
+
+const line_3 = "JavaScript is amazing";
+console.log(line_3);
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const convertedNum = parseInt(firstArg);
+
+if (!isNaN(convertedNum)) {
+	console.log(`My number: ${convertedNum}`);
+} else {
+	console.log("Not a number");
+}
