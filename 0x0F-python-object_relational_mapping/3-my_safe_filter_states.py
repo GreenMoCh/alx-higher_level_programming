345ca33800891c1d2d25f290fd2537064cffa9a8
GreenMoCh <moadchabaa0@gmail.com>
2023-11-19 08:53:29 -0800

    f*ck this project

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,7 +2,6 @@
 """
 This script lists all states from the database
 """
-
 import MySQLdb
 import sys
 
@@ -10,10 +9,4 @@ import sys
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,7 +2,6 @@
 """
 This script lists all states from the database
 """
-
 import MySQLdb
 import sys
 
@@ -11,7 +10,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN ON states.id=cities.state_id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
